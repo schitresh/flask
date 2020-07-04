1b341e9eac158299ed84f862977a7292396b78c9
Chitresh <chitresh.hsharma@gmail.com>
2020-07-04 20:19:23 +0530

    initial commit

---
+++ blog/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pdb
+
+app = Flask(__name__)
+app.debug = True
+def before_request(): app.jinja_env.cache = {}
+app.before_request(before_request)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(50))
+  subtitle = db.Column(db.String(50))
+  author = db.Column(db.String(20))
+  date_posted = db.Column(db.DateTime)
+  content = db.Column(db.Text)
+
+@app.route('/')
+def index():
+  posts = Post.query.order_by(Post.date_posted.desc()).all()
+  return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about(): return render_template('about.html')
+
+@app.route('/contact')
+def contact(): return render_template('contact.html')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+  post = Post.query.filter_by(id=post_id).one()
+  return render_template('post.html', post=post)
+
+@app.route('/add')
+def add(): return render_template('add.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+  # pdb.set_trace()
+  title = request.form['title']
+  subtitle = request.form['subtitle']
+  author = request.form['author']
+  content = request.form['content']
+
+  post = Post(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+  db.session.add(post)
+  db.session.commit()
+
+  return redirect(url_for('index'))
+
+if __name__ == '__main__':
+  app.debug = True
+  app.jinja_env.cache = {}
+  app.jinja_env.auto_reload = True
+  app.config['TEMPLATES_AUTO_RELOAD'] = True
+  app.run(debug=True, use_reloader=True)
